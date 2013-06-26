23641d710f7ad0f7e9f28813595402e93bf8569b
Ben Kauffman <bdkauff@gmail.com>
2013-06-25 20:39:01 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var Leap = require('leapjs').Leap
+
+
+
+// the ExpressJS App
+var app = express();
+
+// configuration of port, templates (/views), static files (/public)
+// and other expressjs settings for the web server.
+app.configure(function(){
+
+  // server port number
+  app.set('port', process.env.PORT || 5000);
+
+  //  templates directory to 'views'
+  app.set('views', __dirname + '/views');
+
+  // setup template engine - we're using Hogan-Express
+  app.set('view engine', 'html');
+  app.set('layout','layout');
+  app.engine('html', require('hogan-express')); // https://github.com/vol4ok/hogan-express
+
+  app.use(express.favicon());
+  // app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // database - skipping until week 5
+  //app.db = mongoose.connect(process.env.MONGOLAB_URI);
+  
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+// ROUTES
+var routes = require('./routes/index.js');
+app.get('/', routes.index);
+app.get('/europeana', routes.europeana);
+app.get('/rijks', routes.rijks);
+app.get('/scrapi/random', routes.scrapiRandom);
+
+// create NodeJS HTTP server using 'app'
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,95 @@
+
+var request = require("request");
+
+
+/*
+ * GET home page.
+ */
+
+var siteTitle = "MET template";
+
+exports.index = function(req, res){
+	
+
+	var templateData = {
+
+		title : siteTitle
+	}
+
+  	res.render("index.html", templateData);
+};
+
+exports.europeana = function(req, res) {
+	var baseUrl = "http://europeana.eu/api/v2/search.json?wskey=";
+	var apiKey = process.env.EUROPEANA_API_KEY;
+	var query = "&query=picasso+pablo";
+
+		request.get(baseUrl + apiKey + query, function(err, response, data){
+			if(err) {
+			res.send("There was an error requesting the url.")
+		}
+			var apiData = JSON.parse(data);
+			
+
+			var templateData =  {
+				data : apiData
+			}
+			res.send("index.html", templateData);
+		});
+};
+
+exports.rijks = function(req,res) {
+	var baseUrl = "http://www.rijksmuseum.nl/api/oai/"+process.env.RIJKS_API_KEY+"/?";
+	var query = "verb=ListRecords&set=collectie_online&metadataPrefix=oai_dc";
+	var testUrl = "https://www.rijksmuseum.nl/api/oai/745c39a7-340c-4993-adf3-3e471238d528/?verb=listrecords&metadataPrefix=oai_dc"
+	request.get(testUrl, function(err, response, data) {
+		if(err) {
+			console.log("There was an error requesting the url")
+
+		}
+		var parseString = require('xml2js').parseString;
+		var xml = data;
+		parseString(xml, function(err, result) {
+			//console.log(result);
+			var json = JSON.stringify(result);
+			console.log(json);
+
+			var templateData = {
+			json : json
+			}	
+			res.send('index.html', templateData);
+		});
+	});
+};
+
+exports.scrapiRandom = function(req,res) {
+	
+	var randomUrl = "http://scrapi.org/random?images=true";
+	request.get(randomUrl, function(err,response,data){
+		if(err) {
+			res.send("There was was an error requesting the URL")
+		}
+
+		apiData = JSON.parse(data);
+
+		var templateData = {
+			image : apiData.image
+		}
+		
+		res.render("index.html", templateData);
+	});
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
