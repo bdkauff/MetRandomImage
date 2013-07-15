35bd02a10c1063fc8093870b3c645aace243844c
Ben Kauffman <bdkauff@gmail.com>
2013-07-15 17:03:44 -0400

    fixed random route

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,6 @@
 var request = require("request");
 var async = require("async");
 
-exports.index = function(req, res){
-
-};
-
-exports.europeana = function(req, res) {
-	
-	var baseUrl = "http://europeana.eu/api/v2/search.json?wskey=";
-	var apiKey = process.env.EUROPEANA_API_KEY;
-	var query = "&query=picasso+pablo";
-
-	request.get(baseUrl + apiKey + query, function(err, response, data){
-		if(err) {
-		res.send("There was an error requesting the url.")
-		}
-		var apiData = JSON.parse(data);
-		var templateData =  {
-			data : apiData
-		}
-		res.send("index.html", templateData);
-	});
-};
-
-exports.rijks = function(req,res) {
-	
-	var baseUrl = "http://www.rijksmuseum.nl/api/oai/"+process.env.RIJKS_API_KEY+"/?";
-	var query = "verb=ListRecords&set=collectie_online&metadataPrefix=oai_dc";
-	var testUrl = "https://www.rijksmuseum.nl/api/oai/745c39a7-340c-4993-adf3-3e471238d528/?verb=listrecords&metadataPrefix=oai_dc"
-	
-	request.get(testUrl, function(err, response, data) {
-		if(err) {
-			console.log("There was an error requesting the url")
-		}
-		var parseString = require('xml2js').parseString;
-		var xml = data;
-		parseString(xml, function(err, result) {
-			//console.log(result);
-			var json = JSON.stringify(result);
-			console.log(json);
-			var templateData = {
-			json : json
-			}	
-			res.send('index.html', templateData);
-		});
-	});
-};
-
 exports.scrapiRandom = function(req,res) {
 	//Each "scene" begins with a hit to scrAPI random image request
 	var randomImageUrl = "http://scrapi.org/random?images=true";
@@ -74,8 +28,8 @@ exports.scrapiRandom = function(req,res) {
 
 exports.scrapiQuery = function(req, res) {
 
-	var queryString = "africa";
-	var queryUrl = "http://scrapi.org/ids?query=" + queryString;
+	var queryString = "egypt";
+	var queryUrl = "http://scrapi.org/ids?query="+ queryString + "&images=true";
 
 	request.get(queryUrl, function(err, response, data){
 		if(err) {
@@ -83,13 +37,15 @@ exports.scrapiQuery = function(req, res) {
 		}
 		apiData = JSON.parse(data);
 		console.log(apiData);
-		
-		var templateData = {
-			medium : apiData.Medium,
-			what : apiData.What,
-			whereBroad : apiData.Where[0],
-			whereSpecific : apiData.Where[1],
-			image : apiData.image
+
+		if(apiData.image) {
+			var templateData = {
+				medium : apiData.Medium,
+				what : apiData.What,
+				whereBroad : apiData.Where[0],
+				whereSpecific : apiData.Where[1],
+				image : apiData.image
+			};
 		};
 		console.log(templateData);
 		res.render("index.html", templateData);
